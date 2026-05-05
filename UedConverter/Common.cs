8377a80da325101857874a55d5e80a742fd9ba45
reszy <rafblend@gmail.com>
2026-05-05 11:43:15 +0200

    Small code cleanup and window names refactor

--- UedConverter/Common.cs
+++ UedConverter/Common.cs
@@ -1,35 +1,34 @@
-﻿namespace UedConverter
+﻿ namespace UedConverter;
+
+public static class Common
 {
-    public static class Common
+    public static string GetSizeWithUnits(double value)
     {
-        public static string GetSizeWithUnits(double value)
+        string unit = "B";
+        if (value > 1024)
         {
-            string unit = "B";
+            unit = "kB";
+            value /= 1024;
             if (value > 1024)
             {
-                unit = "kB";
+                unit = "MB";
                 value /= 1024;
                 if (value > 1024)
                 {
-                    unit = "MB";
+                    unit = "GB";
                     value /= 1024;
                     if (value > 1024)
                     {
-                        unit = "GB";
+                        unit = "TB";
                         value /= 1024;
-                        if (value > 1024)
-                        {
-                            unit = "TB";
-                            value /= 1024;
-                        }
                     }
                 }
             }
-            else
-            {
-                return $"{value} {unit}";
-            }
-            return $"{value:F2} {unit}";
         }
+        else
+        {
+            return $"{value} {unit}";
+        }
+        return $"{value:F2} {unit}";
     }
 }
--- UedConverter/MainWindow.xaml.cs
+++ UedConverter/MainWindow.xaml.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
 using UedConverter.Converter;
 using UedConverter.UtxFile;
